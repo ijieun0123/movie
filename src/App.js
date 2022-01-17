2edd1cbc624f3a9454dc7c12319ca236da12ca28
Ji-eun <tori@ryanlab.kr>
2022-01-17 18:42:03 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.scss';
+import Detail from './Components/Detail';
+import Home from './Components/Home';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async() => {
+    const response = await fetch(
+      'https://api.themoviedb.org/3/movie/popular?api_key=d771ee361528f7664dfcdb3fde78920a'
+    );
+    const json = await response.json();
+    setMovies(json.results);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* home */}
+        <Route 
+          exact path="/" 
+          element = {<Home movies={ movies } />} 
+        />
+
+        {/* 상세 페이지 */}
+        <Route 
+          exact path="/Detail/:id" 
+          element = {<Detail movies={ movies } />} 
+        />
+      </Routes>
     </div>
   );
 }
