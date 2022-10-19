f163fb912005abb20ab906c809e844a12c257e60
이지은 <tori@ryanlab.kr>
2022-10-19 20:27:22 +0900

    fix: App.js get api 위치 옮김

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
 import { useState, useEffect } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import { Box } from '@material-ui/core';
 import './App.scss';
 import Detail from './pages/Detail';
 import Home from './pages/Home';
-import { Link, Route, Routes } from 'react-router-dom';
 import Header from './organisms/header';
+
+const BoxStyle = {
+    margin: '0 auto',
+    padding: '0 50px',
+    maxWidth: '1200px'
+}
 
 function App() {
 
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  const getMovies = async() => {
-    const response = await fetch(
-      'https://api.themoviedb.org/3/movie/popular?api_key=d771ee361528f7664dfcdb3fde78920a'
+    const getMovies = async () => {
+        const keyword = 'popular'
+        const url = `https://api.themoviedb.org/3/movie/${keyword}?api_key=${process.env.REACT_APP_MOVIE_API}&page=1`
+        try{
+            const res = await axios.get(url);
+            const movies = res.data.results;
+            setMovies(movies);
+        } catch(e) {
+            console.log(e);
+        }
+    }
+  
+    useEffect(() => {
+        getMovies();
+    }, [])
+
+    return (
+        <Box sx={BoxStyle}>
+            {/* 헤더 */}
+            <Header></Header>
+
+            <Routes>
+              {/* home */}
+              <Route 
+                exact path="/" 
+                element = {<Home />} 
+              />
+
+              {/* 상세 페이지 */}
+              <Route 
+                exact path="/Detail/:id" 
+                element = {<Detail movies={ movies } />} 
+              />
+            </Routes>
+        </Box>
     );
-    const json = await response.json();
-    setMovies(json.results);
-  }
-
-  useEffect(() => {
-    getMovies();
-  }, [])
-
-  return (
-    <div className="App">
-      {/* 헤더 */}
-      <Header></Header>
-
-      <Routes>
-        {/* home */}
-        <Route 
-          exact path="/" 
-          element = {<Home movies={ movies } />} 
-        />
-
-        {/* 상세 페이지 */}
-        <Route 
-          exact path="/Detail/:id" 
-          element = {<Detail movies={ movies } />} 
-        />
-      </Routes>
-    </div>
-  );
 }
 
 export default App;
