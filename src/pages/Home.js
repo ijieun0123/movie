2808764022c419d7160fd36b7c41a3dd30336260
이지은 <tori@ryanlab.kr>
2022-10-19 20:28:33 +0900

    fix: Home / card 분리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,29 @@
-import React from 'react'
-import './Home.scss';
-import { Link, Route, Routes } from 'react-router-dom';
-import { Card, Box, CardContent, CardMedia, Paper, Typography  } from '@material-ui/core';
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Grid } from '@material-ui/core'
+import MovieCard from '../organisms/card'
+import { useSelector } from 'react-redux'
+import Paging from '../organisms/paging'
 
-export default function Home({ movies }) {
-    return (
-        <div className="home">
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems:'start',
-                    justifyContent:'space-between',
-                    flexWrap: 'wrap',
-                    marginBottom:100,
-                    '& > :not(style)': {
-                        m: 1,
-                        width: 280,
-                        height: 'auto',
-                        marginBottom: 25
-                    },
-                }}
-            >
-                {
-                    movies.map((movie, i) => {
-                        return(
-                            <Link to={ '/Detail/' + movie.id } className="link" key={i}>
-                                <Card sx={{ maxWidth: 345 }}>
-                                    <CardMedia
-                                        component="img"
-                                        height="160"
-                                        image={ 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path }
-                                        alt="movie post"
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="div" color="primary">
-                                            { movie.original_title.length > 10 ? `${movie.original_title.slice(0, 10)}...` : movie.original_title }
-                                        </Typography>
-                                        <Typography variant="body1">
-                                            { movie.overview.length > 80 ? `${movie.overview.slice(0, 80)}...` : movie.overview }
-                                        </Typography>
-                                    </CardContent>
-                                    <CardContent style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                        <Typography variant="body2">
-                                            { movie.release_date }
-                                        </Typography>
-                                        <Typography variant="body2">
-                                            { movie.vote_average }
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Link>
-                        )
-                    })
-                }
-            </Box>
-        </div>
-    )
+const Home = () => {
+  const movies = useSelector((state) => state.movies.movies)
+
+  return (
+    <>
+      <Grid container justifyContent="center" alignItems="center" spacing={2}>
+        {movies.map((movie, i) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+              <Link to={'/Detail/' + movie.id} className="link">
+                <MovieCard movie={movie} />
+              </Link>
+            </Grid>
+          )
+        })}
+      </Grid>
+      <Paging />
+    </>
+  )
 }
+
+export default Home
