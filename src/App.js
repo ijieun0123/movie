7a14396bb3d108479e98a0cdd0add849d82bae43
이지은 <tori@ryanlab.kr>
2022-10-20 17:34:29 +0900

    test: 계정변경 테스트

--- src/App.js
+++ src/App.js
@@ -1,58 +1,53 @@
-import { useState, useEffect } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
-import { Box } from '@material-ui/core';
-import './App.scss';
-import Detail from './pages/Detail';
-import Home from './pages/Home';
-import Header from './organisms/header';
+import { useState, useEffect } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import { Box } from '@material-ui/core'
+import './App.scss'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+import Header from './organisms/header'
+
+// 깃허브테스트
 
 const BoxStyle = {
-    margin: '0 auto',
-    padding: '0 50px',
-    maxWidth: '1200px'
+  margin: '0 auto',
+  padding: '0 50px',
+  maxWidth: '1200px',
 }
 
 function App() {
+  const [movies, setMovies] = useState([])
 
-    const [movies, setMovies] = useState([]);
-
-    const getMovies = async () => {
-        const keyword = 'popular'
-        const url = `https://api.themoviedb.org/3/movie/${keyword}?api_key=${process.env.REACT_APP_MOVIE_API}&page=1`
-        try{
-            const res = await axios.get(url);
-            const movies = res.data.results;
-            setMovies(movies);
-        } catch(e) {
-            console.log(e);
-        }
+  const getMovies = async () => {
+    const keyword = 'popular'
+    const url = `https://api.themoviedb.org/3/movie/${keyword}?api_key=${process.env.REACT_APP_MOVIE_API}&page=1`
+    try {
+      const res = await axios.get(url)
+      const movies = res.data.results
+      setMovies(movies)
+    } catch (e) {
+      console.log(e)
     }
-  
-    useEffect(() => {
-        getMovies();
-    }, [])
+  }
 
-    return (
-        <Box sx={BoxStyle}>
-            {/* 헤더 */}
-            <Header></Header>
+  useEffect(() => {
+    getMovies()
+  }, [])
 
-            <Routes>
-              {/* home */}
-              <Route 
-                exact path="/" 
-                element = {<Home />} 
-              />
+  return (
+    <Box sx={BoxStyle}>
+      {/* 헤더 */}
+      <Header></Header>
 
-              {/* 상세 페이지 */}
-              <Route 
-                exact path="/Detail/:id" 
-                element = {<Detail movies={ movies } />} 
-              />
-            </Routes>
-        </Box>
-    );
+      <Routes>
+        {/* home */}
+        <Route exact path="/" element={<Home />} />
+
+        {/* 상세 페이지 */}
+        <Route exact path="/Detail/:id" element={<Detail movies={movies} />} />
+      </Routes>
+    </Box>
+  )
 }
 
-export default App;
+export default App
