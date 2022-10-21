fa8a70dc56d43059f7c077f4fc7a3cdbe9088a58
이지은 <ijieun0123@gmail.com>
2022-10-21 20:48:19 +0900

    fix: redux-devTool 사용하기 위한 세팅

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import store from './app/store'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
