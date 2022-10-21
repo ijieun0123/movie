79899812095b3a516754ce6da6910006748861e7
이지은 <ijieun0123@gmail.com>
2022-10-21 20:52:06 +0900

    fix: detail 뷰 분리

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,59 +1,47 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { Grid } from '@material-ui/core'
-import Stack from '@mui/material/Stack'
-import Table from '../organisms/table'
-import DetailCard from '../organisms/detailCard'
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import Navigation from '../molecules/navigation'
+import DetailView from '../organisms/detailView'
+import Video from '../organisms/video'
+import Review from '../organisms/review'
+import Recommend from '../organisms/recommend'
+import useNavi from '../core/useNavi'
 
-const ImgStyle = {
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover',
-  borderRadius: 3,
-  boxShadow:
-    '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
-}
+const Detail = () => {
+  const {id} = useParams()
 
-export default function Detail() {
-  const { id } = useParams()
+  const {value, onChangeTabs} = useNavi()
 
-  const [detail, setDetail] = useState([])
-  const [genres, setGenres] = useState([])
+  const valueArr = ['detail', 'recommend', 'review', 'video']
 
-  const getDetail = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=d771ee361528f7664dfcdb3fde78920a`
-    )
-    const json = await response.json()
-    setDetail(json)
-    setGenres(json.genres)
+  const PrintDetail = () => {
+    if (value === 0) {
+      return <DetailView id={id} />
+    } else if (value === 1) {
+      return <Recommend id={id} />
+    } else if (value === 2) {
+      return <Review id={id} />
+    } else {
+      return <Video id={id} />
+    }
   }
 
   useEffect(() => {
-    getDetail()
-  }, [])
+    PrintDetail()
+  }, [value])
 
   return (
-    <Grid container spacing={2} alignItems="stretch">
-      <Grid item xs={12} md={6} lg={5}>
-        <img
-          style={ImgStyle}
-          src={'https://image.tmdb.org/t/p/w500' + detail.backdrop_path}
-          alt="이미지"
-        />
-      </Grid>
+    <>
+      <Navigation
+        value={value}
+        valueArr={valueArr}
+        onChangeTabs={onChangeTabs}
+      />
 
-      <Grid item xs={12} md={6} lg={7}>
-        <Stack spacing={2}>
-          <DetailCard title={detail.original_title} detail={detail.overview} />
-
-          <Table
-            rating={detail.vote_average}
-            release_date={detail.release_date}
-            genres={genres}
-          />
-        </Stack>
-      </Grid>
-    </Grid>
+      {/* 디테일 뷰 */}
+      {PrintDetail()}
+    </>
   )
 }
+
+export default Detail
