15c4a47cb2a7ef9344c20e02fce08848fb1272fd
이지은 <ijieun0123@gmail.com>
2022-10-21 20:54:45 +0900

    fix: header navigation 분리

--- src/organisms/header.js
+++ src/organisms/header.js
@@ -1,37 +1,29 @@
-import { Link } from 'react-router-dom';
-import { 
-    Typography,
-    Tabs,
-    Tab,
-    Box,
-} from '@material-ui/core';
-import Navigation from '../molecules/navigation';
+import {Link} from 'react-router-dom'
+import {Typography, Tabs, Tab, Box} from '@material-ui/core'
 
 const Header = () => {
-    return (
-        <>
-            {/* 로고 */}
-            <Box sx={{ 
-                padding:'20px 0 10px',
-                textAlign: "center"
-            }}>
-                <Typography 
-                    gutterBottom 
-                    variant="h2" 
-                    component="div" 
-                    color="primary" 
-                    style={{ 
-                        fontWeight: 'bold'
-                    }}
-                >
-                    <Link to="/" className="link">Movie</Link>
-                </Typography>
-            </Box>
-            
-            {/* 네비게이션 */}
-            <Navigation />
-        </>
-    )
+  return (
+    <Box
+      sx={{
+        padding: '20px 0 10px',
+        textAlign: 'center',
+      }}
+    >
+      <Typography
+        gutterBottom
+        variant="h2"
+        component="div"
+        color="primary"
+        style={{
+          fontWeight: 'bold',
+        }}
+      >
+        <Link to="/" className="link">
+          Movie
+        </Link>
+      </Typography>
+    </Box>
+  )
 }
 
-export default Header;
+export default Header
