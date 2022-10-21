249796646d2348caae504715dd01973899955a5d
이지은 <ijieun0123@gmail.com>
2022-10-21 20:54:23 +0900

    fix: paging 수정

--- src/organisms/paging.js
+++ src/organisms/paging.js
@@ -1,32 +1,11 @@
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { PAGE } from '../features/moviesSlice'
 import Pagination from '@mui/material/Pagination'
 import Stack from '@mui/material/Stack'
 
-const Paging = () => {
-  const value = useSelector((state) => state.movies.value)
-
-  const [page, setPage] = useState(1)
-
-  const dispatch = useDispatch()
-
-  const onChangePage = (e, newValue) => {
-    setPage(newValue)
-  }
-
-  useEffect(() => {
-    setPage(1)
-  }, [value])
-
-  useEffect(() => {
-    dispatch(PAGE(page))
-  }, [page])
-
+const Paging = ({page, count, onChangePage}) => {
   return (
     <Stack m={10} spacing={2} justifyContent="center" alignItems="center">
       <Pagination
-        count={10}
+        count={count > 10 ? 10 : count}
         color="primary"
         page={page}
         onChange={onChangePage}
