f661a62f8b909efdebe9c3f32858f0008837b151
이지은 <ijieun0123@gmail.com>
2022-10-21 20:51:26 +0900

    fix: getMovies 호출, 네비게이션, 페이징 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,66 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Grid } from '@material-ui/core'
+/*
+* Home Page
+1. 구성
+  - navigation
+    => 'now_playing', 'popular', 'top_rated', 'upcoming'
+  - movieCard Lists 
+    => getMovies API
+  - Paging 
+
+2. 주요기능
+  - 네비게이션, 페이지 클릭시 movie lists => 스토어에 저장, 화면에 띄우기
+*/
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import {Grid} from '@material-ui/core'
 import MovieCard from '../organisms/movieCard'
-import { useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import Paging from '../organisms/paging'
+import Navigation from '../molecules/navigation'
+import {MOVIES} from '../features/moviesSlice'
+import api from '../axios/api'
+import useNavi from '../core/useNavi'
+import usePage from '../core/usePage'
 
 const Home = () => {
-  const movies = useSelector((state) => state.movies.movies)
+  const movies = useSelector(state => state.movies.movies)
+
+  const [count, setCount] = useState(0)
+
+  const {value, onChangeTabs} = useNavi()
+  const {page, onChangePage} = usePage(value)
+
+  const valueArr = ['now_playing', 'popular', 'top_rated', 'upcoming']
+
+  const dispatch = useDispatch()
+
+  const getMovies = () => {
+    const keyword = valueArr[value]
+    api
+      .getMovies(keyword, page)
+      .then(res => {
+        console.log(res.data)
+        const {results, total_pages} = res.data
+        setCount(total_pages)
+        dispatch(MOVIES(results))
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [page, value])
 
   return (
     <>
+      <Navigation
+        value={value}
+        valueArr={valueArr}
+        onChangeTabs={onChangeTabs}
+      />
+
       <Grid container justifyContent="center" alignItems="center" spacing={2}>
         {movies.map((movie, i) => {
           return (
@@ -21,7 +72,8 @@ const Home = () => {
           )
         })}
       </Grid>
-      <Paging />
+
+      <Paging page={page} count={count} onChangePage={onChangePage} />
     </>
   )
 }
