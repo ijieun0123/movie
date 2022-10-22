4eeafc3abe361bdbf93b694545706176ecf47b54
이지은 <ijieun0123@gmail.com>
2022-10-22 18:10:41 +0900

    fix: Container 스타일 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {useState, useEffect} from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
-import axios from 'axios'
-import {Container} from '@material-ui/core'
+import {Container, Box} from '@material-ui/core'
 import './App.scss'
 import Detail from './pages/Detail'
 import Home from './pages/Home'
 import Header from './organisms/header'
 
+const ContainerStyle = {
+  paddingTop: 50,
+  paddingBottom: 100,
+}
+
 function App() {
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="lg" style={ContainerStyle}>
       {/* 헤더 */}
       <Header></Header>
 
