8d01191fad06f761d6870340ad5f5407ae187cb8
이지은 <ijieun0123@gmail.com>
2022-10-22 16:40:08 +0900

    fix: review UI 생성 ( reviewCard + Paging )

---
+++ src/molecules/reviewCard.js
@@ -0,0 +1,73 @@
+import {useState} from 'react'
+import {blue} from '@mui/material/colors'
+import Avatar from '@mui/material/Avatar'
+import Star from '../atoms/star'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import {Card, CardHeader, CardContent, Box, Typography} from '@material-ui/core'
+
+const CardStyle = {
+  position: 'relative',
+  height: '100%',
+}
+
+const ShortenStyle = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  display: '-webkit-box',
+  WebkitLineClamp: 5,
+  WebkitBoxOrient: 'vertical',
+}
+
+const ExpandMoreIconStyle = {
+  position: 'absolute',
+  color: '#888',
+  right: 15,
+  bottom: 15,
+  cursor: 'pointer',
+}
+
+const ReviewCard = ({author, avatarPath, content, updatedAt, rating}) => {
+  const [open, setOpen] = useState(false)
+
+  const onClickMore = () => setOpen(!open)
+
+  return (
+    <Card style={CardStyle}>
+      <CardHeader
+        avatar={
+          avatarPath ? (
+            <Avatar
+              alt="Remy Sharp"
+              src={'https://image.tmdb.org/t/p/w500' + avatarPath}
+            />
+          ) : (
+            <Avatar sx={{bgcolor: blue[500]}} aria-label="recipe">
+              {author.slice(0, 1)}
+            </Avatar>
+          )
+        }
+        title={author}
+        subheader={updatedAt}
+      />
+
+      <CardContent>
+        <Typography variant="body2" style={open ? null : ShortenStyle}>
+          {content}
+        </Typography>
+      </CardContent>
+
+      <Box sx={{ml: 2, mb: 2}}>
+        <Star rating={rating} size="medium" />
+      </Box>
+
+      {open ? (
+        <ExpandLessIcon onClick={onClickMore} sx={ExpandMoreIconStyle} />
+      ) : (
+        <ExpandMoreIcon onClick={onClickMore} sx={ExpandMoreIconStyle} />
+      )}
+    </Card>
+  )
+}
+
+export default ReviewCard
--- src/organisms/review.js
+++ src/organisms/review.js
@@ -2,10 +2,12 @@ import * as React from 'react'
 import {useState, useEffect} from 'react'
 import api from '../axios/api'
 import usePage from '../core/usePage'
+import ReviewCard from '../molecules/reviewCard'
+import {Grid} from '@material-ui/core'
+import Paging from '../molecules/paging'
 
 const Review = ({id}) => {
   const [reviews, setReviews] = useState([])
-
   const [count, setCount] = useState(0)
 
   const {page, onChangePage} = usePage(null)
@@ -28,7 +30,25 @@ const Review = ({id}) => {
     getReview()
   }, [page])
 
-  return <></>
+  return (
+    <>
+      <Grid container spacing={2} alignItems="stretch">
+        {reviews.map(review => (
+          <Grid key={review.id} item xs={12} md={6}>
+            <ReviewCard
+              author={review.author}
+              avatarPath={review.author_details.avatar_path}
+              content={review.content}
+              updatedAt={review.updated_at}
+              rating={review.author_details.rating}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
+      <Paging page={page} count={count} onChangePage={onChangePage} />
+    </>
+  )
 }
 
 export default Review
