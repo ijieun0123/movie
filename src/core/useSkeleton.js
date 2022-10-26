f8a7c9be9b6ad5f8cef2f72dbf6218c04192f481
이지은 <ijieun0123@gmail.com>
2022-10-26 21:17:20 +0900

    add: useSkeleton 훅 생성

---
+++ src/core/useSkeleton.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react'
+
+const useSkeleton = getData => {
+  const [loading, setLoading] = useState(true)
+  const [isSubscribed, setIsSubscribed] = useState(true)
+
+  useEffect(() => {
+    if (!loading) return
+
+    if (isSubscribed)
+      setTimeout(() => {
+        getData()
+        setLoading(false)
+      }, 1500)
+  }, [loading])
+
+  useEffect(() => {
+    getData()
+    return () => setIsSubscribed(false)
+  }, [])
+
+  return {loading}
+}
+
+export default useSkeleton
